498f476bafdc0d28342a0495375ab04180abca74
Sergio-Adarve <sergio.adarve@gmail.com>
2025-01-08 12:09:42 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function generarExcusa() {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+    
+        let excusa = who[Math.floor(Math.random() * who.length)] + ' ' +
+                     action[Math.floor(Math.random() * action.length)] + ' ' +
+                     what[Math.floor(Math.random() * what.length)] + ' ' +
+                     when[Math.floor(Math.random() * when.length)];
+    
+        document.getElementById('excuse').innerHTML = excusa;
+    }
+    
+    window.onload = generarExcusa;
